6a433b877b902ea76f5c64c9976e1fd4ed4ac4ab
Ephraim Basubi <basubiephrem@gmail.com>
2021-04-02 22:09:57 +0300

    data validation

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const userController = require('../controllers/user.controller');
+const userController = require("../controllers/user.controller");
+const {
+  userValidation,
+} = require("../middlewares/user.credentials.validation");
 
 const router = express.Router();
-router.post('/sign-up',userController.signup);
+router.post("/sign-up", userValidation, userController.signup);
 
 module.exports = router;
