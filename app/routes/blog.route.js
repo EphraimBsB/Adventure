b9b3afe879515cc26b4dd75e53a8b680382f627e
Ephraim <basubiephrem@gmail.com>
2021-04-14 01:05:57 +0300

    check the user authorization using token

--- app/routes/blog.route.js
+++ app/routes/blog.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { blogController } = require('../controllers');
+const checkAuth = require('../middlewares/check.auth');
 const { postValidation } = require('../middlewares/post.validation');
 
 const route = express.Router();
-route.post('/post',postValidation ,blogController.save);
+route.post('/post',postValidation, checkAuth, blogController.save);
 route.get('/viewAll',blogController.viewAll);
 route.get('/view/:id',blogController.view);
-route.patch('/update/:id',postValidation,blogController.update);
-route.delete('/delete/:id',blogController.destroy);
+route.patch('/update/:id',postValidation, checkAuth, blogController.update);
+route.delete('/delete/:id',checkAuth,blogController.destroy);
 
 module.exports = route;
