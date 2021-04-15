ae2c31800c997c2ac7cc1ca3f14aa5c2aa3ff45a
Ephraim <basubiephrem@gmail.com>
2021-04-15 22:09:29 +0300

    feature refactor

--- app/routes/blog.route.js
+++ app/routes/blog.route.js
@@ -3,10 +3,10 @@ const { blogController } = require('../controllers');
 const { postValidation } = require('../middlewares/post.validation');
 
 const route = express.Router();
-route.post('/post',postValidation ,blogController.save);
-route.get('/viewAll',blogController.viewAll);
-route.get('/view/:id',blogController.view);
-route.patch('/update/:id',postValidation,blogController.update);
-route.delete('/delete/:id',blogController.destroy);
+route.post('/blog',postValidation ,blogController.save);
+route.get('/blogs',blogController.viewAll);
+route.get('/blog/:id',blogController.view);
+route.patch('/blog/:id',postValidation,blogController.update);
+route.delete('/blog/:id',blogController.destroy);
 
 module.exports = route;
